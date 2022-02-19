9458696ad72dbce2d52f2ca5ec744cd0cb0a7257
Olga Grabek <olga.grabek@twipemobile.com>
2022-02-19 11:47:32 +0100

    Header desktop styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components/macro";
 import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from "../../constants";
+import { FAMILIES, QUERIES } from "../../constants";
 
 import MaxWidthWrapper from "../MaxWidthWrapper";
 import Logo from "../Logo";
@@ -11,29 +11,57 @@ import Button from "../Button";
 const Header = () => {
   return (
     <header>
-      <SuperHeader>
-        <Row>
-          <ActionGroup>
-            <button>
-              <Search size={24} />
-            </button>
-            <button>
-              <Menu size={24} />
-            </button>
-          </ActionGroup>
-          <ActionGroup>
-            <button>
-              <User size={24} />
-            </button>
-          </ActionGroup>
-        </Row>
-      </SuperHeader>
-      <MainHeader>
-        <Logo />
-      </MainHeader>
+      <MobileLayout>
+        <SuperHeader>
+          <Row>
+            <ActionGroup>
+              <button>
+                <Search size={24} />
+              </button>
+              <button>
+                <Menu size={24} />
+              </button>
+            </ActionGroup>
+            <ActionGroup>
+              <button>
+                <User size={24} />
+              </button>
+            </ActionGroup>
+          </Row>
+        </SuperHeader>
+        <MainHeader>
+          <Logo />
+        </MainHeader>
+      </MobileLayout>
+
+      <DesktopLayout>
+        <ActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </ActionGroup>
+        <MainHeader>
+          <Logo />
+        </MainHeader>
+        <SecondaryActionGroupWrapper>
+          <SecondaryActionGroup>
+            <SubscribeButton>Subcribe</SubscribeButton>
+            <Link href="http://onet.pl">Already a subscriber?</Link>
+          </SecondaryActionGroup>
+        </SecondaryActionGroupWrapper>
+      </DesktopLayout>
     </header>
   );
 };
+
+const MobileLayout = styled.div`
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
+`;
 
 const SuperHeader = styled.div`
   padding: 16px 0;
@@ -57,6 +85,10 @@ const ActionGroup = styled.div`
   svg {
     display: block;
   }
+
+  @media ${QUERIES.desktopAndUp} {
+    align-self: center;
+  }
 `;
 
 const MainHeader = styled(MaxWidthWrapper)`
@@ -70,6 +102,43 @@ const MainHeader = styled(MaxWidthWrapper)`
     margin-top: 42px;
     margin-bottom: 72px;
   }
+
+  @media ${QUERIES.desktopAndUp} {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+`;
+
+const DesktopLayout = styled(MaxWidthWrapper)`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: grid;
+    grid-template-columns: 1fr 2fr 1fr;
+    margin-bottom: 83px;
+    margin-top: 12px;
+  }
+`;
+
+const SecondaryActionGroupWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-end;
+`;
+
+const SecondaryActionGroup = styled.div`
+  text-align: center;
+`;
+
+const Link = styled.a`
+  font: 0.875rem/1.7 ${FAMILIES.serif};
+  font-style: italic;
+  text-decoration: underline;
+`;
+
+const SubscribeButton = styled(Button)`
+  margin-bottom: 5px;
 `;
 
 export default Header;
